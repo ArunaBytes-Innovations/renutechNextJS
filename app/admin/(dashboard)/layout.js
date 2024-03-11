0bc21b51683744a5d02102e0f82f6d173d9d4de3
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2024-03-11 12:32:47 +0530

    dashboard layout

---
+++ app/admin/(dashboard)/layout.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const AdminLayout = ({ children }) => {
+    return (
+        <div className="flex flex-col md:flex-row h-screen">
+            {/* Sidebar */}
+            <aside className="bg-gray-800 text-gray-100 w-full md:w-64 flex-shrink-0">
+                {/* Sidebar content */}
+                Sidebar
+            </aside>
+
+            {/* Main content */}
+            <div className="flex flex-col flex-1">
+                {/* Header */}
+                <header className="bg-white shadow-md">
+                    {/* Header content */}
+                    Header
+                </header>
+
+                {/* Main body */}
+                <main className="flex-1 overflow-y-auto p-4">
+                    {/* Main body content (children components) */}
+                    {children}
+                </main>
+
+                {/* Footer */}
+                <footer className="bg-gray-200 shadow-md">
+                    {/* Footer content */}
+                    Footer
+                </footer>
+            </div>
+        </div>
+    );
+};
+
+export default AdminLayout;
---
+++ app/admin/login/page.jsx
@@ -0,0 +1,82 @@
+"use client";
+import { useState } from "react";
+
+const AdminLoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Add your login logic here, e.g., sending login credentials to the server
+    console.log(
+      "Logging in with username:",
+      username,
+      "and password:",
+      password
+    );
+    setPassword("");
+    setUsername("");
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-300 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
+            Admin Login
+          </h2>
+        </div>
+        <form onSubmit={handleLogin} className="mt-8 space-y-6">
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="username" className="sr-only">
+                Username
+              </label>
+              <input
+                id="username"
+                name="username"
+                type="text"
+                autoComplete="username"
+                required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Username"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Login
+            </button>
+            <div className="text-center m-1 font-thin">
+              &copy; ArunaBytes 2024
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLoginPage;
