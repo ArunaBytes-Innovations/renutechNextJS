aea2a233a995ea0d64de0aad55b5cb303b97f159
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2024-03-17 18:35:37 +0530

    studentsById

---
+++ app/api/students/[id]/route.js
@@ -0,0 +1,42 @@
+// api/students/[id]/route.js
+
+import { NextResponse } from "next/server";
+import connectDB from "@/app/lib/connectDB";
+import Student from "@/app/models/student";
+
+// getStudentById
+export async function GET(req, { params }) {
+    await connectDB();
+    const id = params.id;
+    const student = await Student.findById(id);
+    return NextResponse.json(student);
+}
+
+// updateStudentById
+export async function PUT(req, { params }) {
+    await connectDB();
+    const id = params.id;
+    const student = await Student.findById(id);
+
+    if (!student) {
+        return NextResponse.error("Student not found", 404);
+    }
+    const updatedStudent = await req.json();
+    Object.assign(student, updatedStudent);
+    await student.save();
+    return NextResponse.json(student);
+}
+
+// deleteStudentById
+export async function DELETE(req, { params }) {
+    await connectDB();
+    const id = params.id;
+    const student = await Student.findById(id);
+
+    if (!student) {
+        return NextResponse.error("Student not found", 404);
+    }
+    await Student.findByIdAndDelete(id);
+
+    return NextResponse.json({ message: "Student deleted" });
+}
