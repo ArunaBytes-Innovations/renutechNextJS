e9747b4d6ee92f2b58a2a0c192a8a0be7989f5a0
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2024-03-20 21:07:00 +0530

    protected event route

--- app/api/events/[id]/route.js
+++ app/api/events/[id]/route.js
@@ -3,8 +3,30 @@ import { NextResponse } from "next/server";
 import connectDB from "@/app/lib/connectDB";
 import Event from "@/app/models/Event";
 
+import jwt from "jsonwebtoken"
+import { headers } from "next/headers";
+
 // getEventById
 export async function GET(req, { params }) {
+    const headersList = headers();
+    const referer = headersList.get("authorization");
+
+    // verify jwt token 
+    const token = referer.split(" ")[1];
+
+    // Check if token is missing
+    if (!token) {
+        return NextResponse.json({ message: 'Unauthorized: Missing token' });
+    }
+
+    // Verify the token
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        console.log('Decoded:', decoded);  // Log the decoded token
+    } catch (err) {
+        console.error('JWT verification error:', err);  // Log the error
+        return NextResponse.json({ message: 'Unauthorized: Invalid token' });
+    }
     await connectDB();
     const id = params.id;
     const event = await Event.findById(id);
@@ -13,6 +35,25 @@ export async function GET(req, { params }) {
 
 // updateEventById
 export async function PUT(req, { params }) {
+    const headersList = headers();
+    const referer = headersList.get("authorization");
+
+    // verify jwt token 
+    const token = referer.split(" ")[1];
+
+    // Check if token is missing
+    if (!token) {
+        return NextResponse.json({ message: 'Unauthorized: Missing token' });
+    }
+
+    // Verify the token
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        console.log('Decoded:', decoded);  // Log the decoded token
+    } catch (err) {
+        console.error('JWT verification error:', err);  // Log the error
+        return NextResponse.json({ message: 'Unauthorized: Invalid token' });
+    }
     await connectDB();
     const id = params.id;
     const event = await Event.findById(id).populate("coordinators");
@@ -28,6 +69,25 @@ export async function PUT(req, { params }) {
 
 // deleteEventById
 export async function DELETE(req, { params }) {
+    const headersList = headers();
+    const referer = headersList.get("authorization");
+
+    // verify jwt token 
+    const token = referer.split(" ")[1];
+
+    // Check if token is missing
+    if (!token) {
+        return NextResponse.json({ message: 'Unauthorized: Missing token' });
+    }
+
+    // Verify the token
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        console.log('Decoded:', decoded);  // Log the decoded token
+    } catch (err) {
+        console.error('JWT verification error:', err);  // Log the error
+        return NextResponse.json({ message: 'Unauthorized: Invalid token' });
+    }
     await connectDB();
     const id = params.id;
     const event = await Event.findById(id);
--- app/api/events/route.js
+++ app/api/events/route.js
@@ -4,6 +4,9 @@ import Event from "@/app/models/Event";
 import Student from "@/app/models/Student";
 import Coordinator from "@/app/models/Coordinator";
 
+import jwt from "jsonwebtoken"
+import { headers } from "next/headers";
+
 export async function GET() {
     await connectDB();
     const event = await Event.find().populate("events.coordinators").populate("coordinators");
@@ -11,6 +14,25 @@ export async function GET() {
 }
 
 export async function POST(req) {
+    const headersList = headers();
+    const referer = headersList.get("authorization");
+
+    // verify jwt token 
+    const token = referer.split(" ")[1];
+
+    // Check if token is missing
+    if (!token) {
+        return NextResponse.json({ message: 'Unauthorized: Missing token' });
+    }
+
+    // Verify the token
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        console.log('Decoded:', decoded);  // Log the decoded token
+    } catch (err) {
+        console.error('JWT verification error:', err);  // Log the error
+        return NextResponse.json({ message: 'Unauthorized: Invalid token' });
+    }
     await connectDB();
     const newEvent = await req.json();
     const event = new Event(newEvent);
