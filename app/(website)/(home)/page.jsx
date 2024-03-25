5f1bad0e8e8500fa77568a985f9f8ba2eaeda13d
Aarju <aarjuelectrical@gmail.com>
2024-03-26 02:15:39 +0530

    Edited some mistake and added the comments

--- app/(website)/(home)/page.jsx
+++ app/(website)/(home)/page.jsx
@@ -1,37 +1,45 @@
-import Navbar from "@/components/web/Navbar";
-import React from "react";
+import Navbar from "@/components/web/Navbar"; // Importing Navbar component
+import React from "react"; // Importing React library
 
+// Defining Home functional component
 const Home = () => {
   return (
     <div className="home-container bg-gradient-to-t from-[#9fd4eb] from-10% via-[#5abeea] via-30% to-[#000000] to-90% w-full min-h-screen">
-      {/* hero section  */}
+      {/* Navbar component */}
       <Navbar />
       <div className="relative -mt-20">
+        {/* Hero section */}
         <div className="hero md:flex justify-between">
           <div className="py-24 md:px-10 w-fit text-white mt-20 text-center md:text-left">
+            {/* Logo */}
             <img
               id="animation"
               src="/assets/renuTech.svg"
               className=" relative z-10 px-10"
               alt=""
             />
+            {/* Event date */}
             <p className="relative z-50 text-xl text-center font-bold md:text-center md:text-2xl md:px-10 pt-6 text-amber-100 capitalize ">
-              08-10 October 2021
+              08-10 April 2024
             </p>
+            {/* Event description */}
             <p className="md:text-xl text-center font-bold md:text-left md:px-10 pt-10 text-amber-100 capitalize ">
               Bytes & Beats Together <br />
               Where Tech innovation meets cultural celebration
             </p>
+            {/* Get Started button */}
             <button className="bg-amber-100 text-sm md:text-lg font-semibold text-black md:ml-10 px-4 py-2 mt-5 rounded-lg">
               Get Started
             </button>
           </div>
+          {/* Animated stars */}
           <img
             className="absolute top-24 md:top-36 animate-pulse w-screen rotate-180"
             src="/assets/stars.png"
             alt="star"
           />
           <div className="md:basis-1/4 -mt-40 md:mt-40 overflow-hidden ">
+            {/* Animated moon */}
             <img
               className="w-54 md:w-full md:h-full h-48 animate-spin"
               src="/assets/moon_full.png"
@@ -42,10 +50,12 @@ const Home = () => {
         </div>
       </div>
 
+      {/* About section */}
       <div className="about bg-inherit py-10 px-10">
         <h2 className="text-center text-[#fcf1c5] text-2xl font-bold underline underline-offset-8  decoration-wavy">
           About Fest
         </h2>
+        {/* Description of RenuTech */}
         <p className="text-center font-semibold text-white mt-5 md:px-40">
           RenuTech is the ultimate fusion of tech and creativity organized by{" "}
           <span className="text-lg text-[#fcf1c5]">SPNREC, Araria!</span>{" "}
@@ -57,11 +67,14 @@ const Home = () => {
         </p>
       </div>
 
+      {/* Events section */}
       <div className="events bg-inherit text-white py-10">
         <h2 className="text-center text-[#fcf1c5] text-2xl font-bold underline underline-offset-8  decoration-wavy">
           Events
         </h2>
+        {/* List of events */}
         <div className="flex flex-wrap justify-evenly mt-5 px-6 md:px-0">
+          {/* Tech Event */}
           <div className="flex justify-evenly bg-white text-black p-2 rounded-lg">
             <img
               src="/assets/design_quest.svg"
@@ -76,6 +89,7 @@ const Home = () => {
               </p>
             </div>
           </div>
+          {/* Cultural Event */}
           <div className="flex justify-evenly bg-white text-black mt-4 md:mt-0 p-2 rounded-lg">
             <img
               src="/assets/cultural_img.png"
